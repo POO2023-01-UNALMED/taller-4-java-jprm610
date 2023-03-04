d3e72ef5bb314c9250e46dea7be1aad5eb8ff1f0
jprm610 <jprm610@gmail.com>
2023-03-04 18:57:36 -0500

    1

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,13 +2,16 @@ package classroom;
 
 public class Persona {
 
-    final long cedula;
+    long cedula;
     String nombre;
     static int totalPersonas;
     
     static {
         totalPersonas = 0;
-        cedula = 3;
+    }
+
+    public Persona() {
+        totalPersonas++;
     }
 
     public Persona(long cedula, String nombre) {
@@ -30,6 +33,7 @@ public class Persona {
     }
 
     public Persona(String nombre) {
+        this.cedula = 1;
         this.nombre = "";
         totalPersonas++;
     }
